984218ec4c0ed4ca2555c099594fc94f224d1f97
Chitra35006 <ch18.cse@gmail.com>
2025-05-07 20:19:49 +0600

    solution of p2

--- all-a8.ts
+++ all-a8.ts
@@ -11,3 +11,27 @@ function formatString(input: string, toUpper?: boolean):string{
 console.log(formatString("Hello")     )
 console.log(formatString("Hello", true))
 console.log(formatString("Hello", false))
+
+type itemList={
+    title: string; 
+    rating: number
+}
+function filterByRating(items: itemList[]): itemList[]{
+    const result: itemList[] =[];
+
+    for(const item of items){
+        if(item.rating > 4){
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+  ];
+  
+  console.log(filterByRating(books));
+  
